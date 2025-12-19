5cb1fc85e61ca217edca6fe3c4adc2976561575c
Aodemi <mattisbbeaupre@gmail.com>
2025-12-19 13:51:15 +0000

    retrait de Lombok

--- src/api/src/main/java/com/mariadb/todo/domain/Task.java
+++ src/api/src/main/java/com/mariadb/todo/domain/Task.java
@@ -2,15 +2,40 @@ package com.mariadb.todo.domain;
 
 import jakarta.persistence.*;
 
-import lombok.Data;
-
-@Data
 @Entity
 @Table(name = "tasks")
 public class Task {
-    @Id 
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String description;
+
     private Boolean completed = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
+
